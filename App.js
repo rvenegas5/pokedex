7a60446f6cf7a0046e8f007720e02d8507db4e63
William Venegas <rvenegas@espol.edu.ec>
2022-01-04 21:04:05 -0500

    Working with Tab Navigator and Drawer Navigator

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+import 'react-native-gesture-handler';
 import React from "react";
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationStack from "./src/navigation/NavigationStack";
+import NavigationTab from "./src/navigation/NavigationTab";
+import NavigationDrawer from './src/navigation/NavigationDrawer';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <NavigationStack />
+      {/* <NavigationStack /> */}
+      {/* <NavigationTab /> */}
+      <NavigationDrawer />
     </NavigationContainer>
   );
 }
